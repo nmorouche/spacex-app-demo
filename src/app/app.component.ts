553e8c6abb912fd4e3412e00741668ef380733e7
Nassim <nassim.morouche1@gmail.com>
2019-07-18 13:40:43 +0200

    Added countdown on menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,15 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { Router, RouterEvent } from '@angular/router';
+import { LaunchesService } from './launches/services/launches.service';
+import { Launch } from './launches/models/launch.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
+  launch: Launch;
   selectedPath: string;
   text: any = {
     Year: 'Y',
@@ -56,7 +59,8 @@ export class AppComponent {
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
-    private router: Router
+    private router: Router,
+    private launchesService: LaunchesService
   ) {
     this.initializeApp();
   }
@@ -71,5 +75,25 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+    this.launchesService.getNextLaunch().subscribe(result => {
+      this.launch = result;
+    });
+  }
+
+  setTimer(time: string) {
+    let countDownDate = new Date(time).getTime();
+    // Update the count down every 1 second
+    let x = setInterval(function () {
+      // Get todays date and time
+      let now = new Date().getTime();
+      // Find the distance between now and the count down date
+      let distance = countDownDate - now;
+      // Time calculations for days, hours, minutes and seconds
+      let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      document.getElementById("timer").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s";
+    }, 1000);
   }
 }
--- src/app/launches/resolvers/launch.resolver.ts
+++ src/app/launches/resolvers/launch.resolver.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 import { LaunchesService } from '../services/launches.service';
-import { Launch } from '../Models/launch.model';
+import { Launch } from '../models/launch.model';
 
 @Injectable({
     providedIn: 'root',
